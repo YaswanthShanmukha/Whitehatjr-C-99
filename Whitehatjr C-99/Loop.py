8c58c226b26b6f261c42e190749f6cb0f51d8c9d
YaswanthShanmukha <74482877+YaswanthShanmukha@users.noreply.github.com>
2022-08-23 12:52:53 +0530

    Add files via upload

---
+++ Whitehatjr C-99/Loop.py
@@ -0,0 +1,30 @@
+rows = int(input("Enter how many rows to be printed : "))
+#print(rows)
+""" 
+for i in range(0, rows):   
+        for j in range(0, i + 1):              
+            print("*", end=" ")
+        print() 
+        
+for i in range(0, rows):
+    for j in range(0, i + 1):
+        print(j, end=" ")
+    print()
+
+for i in range(1, rows):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()    
+
+for i in range(0, rows):
+    for j in range(rows, i, - 1):
+        print("*", end=" ")
+    print() 
+
+for i in range(rows, 0, -1):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()    
+
+
+"""
